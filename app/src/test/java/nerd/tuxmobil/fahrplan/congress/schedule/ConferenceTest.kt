abb3558046f73312a16a5de15fbfb4d5b1e0274e
Tobias Preuss <tobias.preuss@googlemail.com>
2020-12-03 19:53:51 +0100

    Increase test coverage for Conference class.

--- app/src/test/java/nerd/tuxmobil/fahrplan/congress/schedule/ConferenceTest.kt
+++ app/src/test/java/nerd/tuxmobil/fahrplan/congress/schedule/ConferenceTest.kt
@@ -8,6 +8,13 @@ import org.junit.Test
 class ConferenceTest {
 
     @Test
+    fun `default values`() {
+        val conference = Conference()
+        assertThat(conference.firstSessionStartsAt).isEqualTo(0)
+        assertThat(conference.lastSessionEndsAt).isEqualTo(0)
+    }
+
+    @Test
     fun `calculateTimeFrame with frab data spanning multiple days`() {
         val opening = createSession("Opening", duration = 30, dateUtc = 1536332400000L) // 2018-09-07T17:00:00+02:00
         val closing = createSession("Closing", duration = 30, dateUtc = 1536504300000L) // 2018-09-09T16:45:00+02:00
@@ -26,10 +33,30 @@ class ConferenceTest {
     }
 
     @Test
+    fun `calculateTimeFrame with frab data spanning a single day in non-chronological order`() {
+        val opening = createSession("Opening", duration = 30, dateUtc = 1536328800000L) // 2018-09-07T16:00:00+02:00
+        val middle = createSession("Middle", duration = 20, dateUtc = 1536332400000L) // 2018-09-07T17:00:00+02:00
+        val closing = createSession("Closing", duration = 30, dateUtc = 1536336000000L) // 2018-09-07T18:00:00+02:00
+        val (firstSessionStartsAt, lastSessionEndsAt) = createConference(opening, closing, middle)
+        assertThat(firstSessionStartsAt).isEqualTo(16 * 60 - 2 * 60) // 16:00h -2h zone offset = 14:00h
+        assertThat(lastSessionEndsAt).isEqualTo(18 * 60 + 30 - 2 * 60) // -> 18:00 -2h zone offset
+    }
+
+    @Test
     fun `calculateTimeFrame with Pentabarf data`() {
         val opening = createSession("Opening", duration = 25, relStartTime = 570) // 09:30
         val closing = createSession("Closing", duration = 10, relStartTime = 1070) // 17:50
         val (firstSessionStartsAt, lastSessionEndsAt) = createConference(opening, closing)
+        assertThat(firstSessionStartsAt).isEqualTo(570) // 09:30
+        assertThat(lastSessionEndsAt).isEqualTo(1080) // 18:00
+    }
+
+    @Test
+    fun `calculateTimeFrame with Pentabarf data in non-chronological order`() {
+        val opening = createSession("Opening", duration = 25, relStartTime = 570) // 09:30
+        val middle = createSession("Middle", duration = 25, relStartTime = 720) // 12:00
+        val closing = createSession("Closing", duration = 10, relStartTime = 1070) // 17:50
+        val (firstSessionStartsAt, lastSessionEndsAt) = createConference(opening, closing, middle)
         assertThat(firstSessionStartsAt).isEqualTo(570) // 09:30
         assertThat(lastSessionEndsAt).isEqualTo(1080) // 18:00
     }
